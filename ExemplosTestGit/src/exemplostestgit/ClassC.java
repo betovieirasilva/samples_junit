a3ba6d90760f83f4c3ddf2a9ac4c5c8948790c77
Gilberto Vieira da Silva <gilbertovieirasilva@hotmail.com>
2014-08-06 14:31:52 -0300

    Adicionado a classe C (versão 01.0.01)

---
+++ ExemplosTestGit/src/exemplostestgit/ClassC.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exemplostestgit;
+
+/**
+ *
+ * @author Gilberto Vieira da Silva
+ */
+public class ClassC {
+ 
+    public void teste(){
+        //faz outra coisa
+    }
+    
+    
+}
